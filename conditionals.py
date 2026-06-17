50b3789e3c168e876015cfa2aa2cc8d9bd769e1a
vikash1602 <vikashvnb2005@gmail.com>
2026-06-17 22:31:16 +0530

    Week 1 Tue: Python conditionals

---
+++ conditionals.py
@@ -0,0 +1,58 @@
+# Check whether a number is positive, negative, or zero.
+
+# num=int(input("Enter a number: "))
+
+# if num > 0:
+#     print(num, "is positive")
+# elif num < 0:
+#     print(num, "is negative")
+# else:
+#     print(num, "is zero")
+
+# Find the largest of three numbers.
+
+# a=int(input("Enter a number: "))
+# b=int(input("Enter a number: "))
+# c=int(input("Enter a number: "))
+
+# if a>=b and a>=c:
+#     print(a, "is largest of three numbers")
+# elif b>=a and b>=c:
+#     print(b, "is largest of three numbers")
+# else:
+#     print(c, "is largest of three numbers")
+
+# Check if a year is a leap year.
+
+# year=int(input("Enter a year:"))
+# if year % 400 == 0:
+#     print("Leap Year")
+# elif year % 100 == 0:
+#     print("Not a Leap Year")
+# elif year % 4 == 0:
+#     print("Leap Year")
+# else:
+#     print("Not a Leap Year")
+
+# Grade calculator (A/B/C/D/F).
+# Grade=int(input("Enter the Grade: "))
+# if Grade>90 and Grade<=100:
+#     print("Grade is A")
+# elif Grade>80 and Grade<=90:
+#     print("Grade is B")
+# elif Grade>70 and Grade<=80:
+#     print("Grade is C")
+# elif Grade>60 and Grade<=70:
+#     print("Grade is D")
+# elif Grade<=60:
+#     print("Grade is F")
+# else:
+#     print("Enter correct Grade")
+
+# Check whether a number is even or odd.
+# n=int(input("Enter a number: "))
+# if n%2==0:
+#     print(n, "is even number")
+# else:
+#     print(n, "is odd number")
+
